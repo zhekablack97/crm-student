1892623ef14385c14655a6eff00271b264b329bc
Морозов Евгений Игоревич <zhekablack97@yandex.ru>
2020-04-08 19:53:45 +0300

    добавление типизации для поля поля имени

--- src/components/Name/Name.tsx
+++ src/components/Name/Name.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import './Name.css';
 
-const Name = () => {
+export interface NameProps{
+  name?: string
+}
+
+
+const Name = (props: NameProps) => {
+  console.log(props);
     return (
       <span>
-        
+        {props.name}
       </span>
     )
 }
